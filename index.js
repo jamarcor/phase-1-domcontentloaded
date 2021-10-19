7204771c9a60eb5fba2ce660d0fe7fe1a5d22fe2
James Correa <james.correa9090@gmail.com>
2021-10-19 06:57:16 -1000

    commit tree 58d8a203371e585c06c3e1865dd2b79e9ce3c8c6

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+        let replacedText = document.getElementById('text')
+        replacedText.textContent = "This is really cool!";
+    });
